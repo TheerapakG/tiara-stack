28f7fd63740558292325eca296c7c9df630af397
TheerapakG <theerapakg@gmail.com>
2025-06-22 20:24:07 +0700

    fix: only export top level for sheet apis

--- packages/sheet-apis/src/index.ts
+++ packages/sheet-apis/src/index.ts
@@ -2,6 +2,7 @@ import { serve as crosswsServe } from "crossws/server/node";
 import { Effect, pipe } from "effect";
 import { serve } from "typhoon-server/server";
 import { server } from "./server";
+export { server };
 
 const serveEffect = pipe(server, Effect.flatMap(serve(crosswsServe)));
 
