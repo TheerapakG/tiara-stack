8ac8fb8edfc7948616a44f6d20644729b41b4215
TheerapakG <theerapakg@gmail.com>
2025-06-30 18:06:14 +0700

    feat: observability

--- packages/sheet-apis/src/index.ts
+++ packages/sheet-apis/src/index.ts
@@ -1,3 +1,7 @@
+import { NodeSdk } from "@effect/opentelemetry";
+import { PrometheusExporter } from "@opentelemetry/exporter-prometheus";
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
+import { BatchSpanProcessor } from "@opentelemetry/sdk-trace-base";
 import { serve as crosswsServe } from "crossws/server/node";
 import { Effect, pipe } from "effect";
 import { InferServerType, serve } from "typhoon-server/server";
@@ -5,6 +9,16 @@ import { server } from "./server";
 
 export type Server = InferServerType<typeof server>;
 
-const serveEffect = pipe(server, Effect.flatMap(serve(crosswsServe)));
+const NodeSdkLive = NodeSdk.layer(() => ({
+  resource: { serviceName: "sheet-apis" },
+  spanProcessor: new BatchSpanProcessor(new OTLPTraceExporter()),
+  metricReader: new PrometheusExporter(),
+}));
+
+const serveEffect = pipe(
+  server,
+  Effect.flatMap(serve(crosswsServe)),
+  Effect.provide(NodeSdkLive),
+);
 
 Effect.runPromise(serveEffect);
