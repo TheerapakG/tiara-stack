6f252fd728fc473297cc7fc7a07ea08c06d09197
TheerapakG <theerapakg@gmail.com>
2025-07-21 23:14:23 +0700

    feat: server managers config

--- packages/sheet-bot/src/commands/chatInputCommands/server.ts
+++ packages/sheet-bot/src/commands/chatInputCommands/server.ts
@@ -4,6 +4,7 @@ import {
   escapeMarkdown,
   InteractionContextType,
   PermissionFlagsBits,
+  roleMention,
   SlashCommandBuilder,
   SlashCommandSubcommandBuilder,
   SlashCommandSubcommandGroupBuilder,
@@ -38,23 +39,38 @@ const handleListConfig = chatInputSubcommandHandlerContextBuilder()
       Effect.bind("guildConfigsSubscription", ({ guild }) =>
         GuildConfigService.getConfig(guild.id),
       ),
+      Effect.bind("managerRolesSubscription", ({ guild }) =>
+        GuildConfigService.getManagerRoles(guild.id),
+      ),
       Effect.bind("guildConfigObserver", ({ guildConfigsSubscription }) =>
         observeOnce(guildConfigsSubscription.value),
+      ),
+      Effect.bind("managerRolesObserver", ({ managerRolesSubscription }) =>
+        observeOnce(managerRolesSubscription.value),
       ),
       Effect.bind(
         "guildConfig",
         ({ guildConfigObserver }) => guildConfigObserver.value,
       ),
+      Effect.bind(
+        "managerRoles",
+        ({ managerRolesObserver }) => managerRolesObserver.value,
+      ),
       Effect.bind("sheetId", ({ guildConfig }) =>
         Option.fromNullable(guildConfig[0].sheetId),
       ),
-      Effect.bind("response", ({ guild, sheetId }) =>
+      Effect.bind("response", ({ guild, sheetId, managerRoles }) =>
         Effect.tryPromise(() =>
           interaction.reply({
             embeds: [
               new EmbedBuilder()
                 .setTitle(`Config for ${escapeMarkdown(guild.name)}`)
-                .setDescription(`Sheet id: ${escapeMarkdown(sheetId)}`),
+                .setDescription(
+                  [
+                    `Sheet id: ${escapeMarkdown(sheetId)}`,
+                    `Manager roles: ${managerRoles.length > 0 ? managerRoles.map((role) => roleMention(role.roleId)).join(", ") : "None"}`,
+                  ].join("\n"),
+                ),
             ],
           }),
         ),
@@ -62,6 +78,126 @@ const handleListConfig = chatInputSubcommandHandlerContextBuilder()
     ),
   )
   .build();
+
+const handleAddManagerRole = chatInputSubcommandHandlerContextBuilder()
+  .data(
+    new SlashCommandSubcommandBuilder()
+      .setName("manager_role")
+      .setDescription("Add a manager role for the server")
+      .addRoleOption((option) =>
+        option
+          .setName("role")
+          .setDescription("The role to add")
+          .setRequired(true),
+      ),
+  )
+  .handler((interaction) =>
+    pipe(
+      Effect.Do,
+      Effect.bindAll(() => ({
+        role: Effect.try(() => interaction.options.getRole("role", true)),
+        guild: Option.fromNullable(interaction.guild),
+        memberPermissions: Option.fromNullable(interaction.memberPermissions),
+      })),
+      Effect.tap(({ memberPermissions }) =>
+        !memberPermissions.has(PermissionFlagsBits.ManageGuild)
+          ? Effect.fail("You do not have permission to manage the server")
+          : Effect.void,
+      ),
+      Effect.tap(({ guild, role }) =>
+        GuildConfigService.addManagerRole(guild.id, role.id),
+      ),
+      Effect.bind("response", ({ guild, role }) =>
+        Effect.tryPromise(() =>
+          interaction.reply({
+            embeds: [
+              new EmbedBuilder()
+                .setTitle(`Success!`)
+                .setDescription(
+                  `${roleMention(role.id)} is now a manager role for ${escapeMarkdown(guild.name)}`,
+                )
+                .setTimestamp()
+                .setFooter({
+                  text: `${interaction.client.user.username} ${process.env.BUILD_VERSION}`,
+                }),
+            ],
+          }),
+        ),
+      ),
+      Effect.asVoid,
+    ),
+  )
+  .build();
+
+const handleAdd =
+  chatInputSubcommandGroupHandlerContextWithSubcommandHandlerBuilder()
+    .data(
+      new SlashCommandSubcommandGroupBuilder()
+        .setName("add")
+        .setDescription("Add a config to the server"),
+    )
+    .addSubcommandHandler(handleAddManagerRole)
+    .build();
+
+const handleRemoveManagerRole = chatInputSubcommandHandlerContextBuilder()
+  .data(
+    new SlashCommandSubcommandBuilder()
+      .setName("manager_role")
+      .setDescription("Remove a manager role from the server")
+      .addRoleOption((option) =>
+        option
+          .setName("role")
+          .setDescription("The role to remove")
+          .setRequired(true),
+      ),
+  )
+  .handler((interaction) =>
+    pipe(
+      Effect.Do,
+      Effect.bindAll(() => ({
+        role: Effect.try(() => interaction.options.getRole("role", true)),
+        guild: Option.fromNullable(interaction.guild),
+        memberPermissions: Option.fromNullable(interaction.memberPermissions),
+      })),
+      Effect.tap(({ memberPermissions }) =>
+        !memberPermissions.has(PermissionFlagsBits.ManageGuild)
+          ? Effect.fail("You do not have permission to manage the server")
+          : Effect.void,
+      ),
+      Effect.tap(({ guild, role }) =>
+        GuildConfigService.removeManagerRole(guild.id, role.id),
+      ),
+      Effect.bind("response", ({ guild, role }) =>
+        Effect.tryPromise(() =>
+          interaction.reply({
+            embeds: [
+              new EmbedBuilder()
+                .setTitle(`Success!`)
+                .setDescription(
+                  `${roleMention(role.id)} is no longer a manager role for ${escapeMarkdown(guild.name)}`,
+                )
+                .setTimestamp()
+                .setFooter({
+                  text: `${interaction.client.user.username} ${process.env.BUILD_VERSION}`,
+                }),
+            ],
+          }),
+        ),
+      ),
+      Effect.asVoid,
+    ),
+  )
+  .build();
+
+const handleRemove =
+  chatInputSubcommandGroupHandlerContextWithSubcommandHandlerBuilder()
+    .data(
+      new SlashCommandSubcommandGroupBuilder()
+        .setName("remove")
+        .setDescription("Remove a config from the server"),
+    )
+    .addSubcommandHandler(handleRemoveManagerRole)
+    .build();
 
 const handleSetSheet = chatInputSubcommandHandlerContextBuilder()
   .data(
@@ -117,12 +253,12 @@ const handleSetSheet = chatInputSubcommandHandlerContextBuilder()
   )
   .build();
 
-const handleSetConfig =
+const handleSet =
   chatInputSubcommandGroupHandlerContextWithSubcommandHandlerBuilder()
     .data(
       new SlashCommandSubcommandGroupBuilder()
-        .setName("set_config")
-        .setDescription("Set config for the server"),
+        .setName("set")
+        .setDescription("Set the config of the server"),
     )
     .addSubcommandHandler(handleSetSheet)
     .build();
@@ -143,6 +279,8 @@ export const command =
           InteractionContextType.PrivateChannel,
         ),
     )
-    .addSubcommandGroupHandler(handleSetConfig)
+    .addSubcommandGroupHandler(handleAdd)
+    .addSubcommandGroupHandler(handleRemove)
+    .addSubcommandGroupHandler(handleSet)
     .addSubcommandHandler(handleListConfig)
     .build();
