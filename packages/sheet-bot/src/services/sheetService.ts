7c9bd707f3ce3ff705e9c29f3a03b1dee8a78477
TheerapakG <theerapakg@gmail.com>
2025-08-06 19:17:27 +0700

    fix: fix filler parser

--- packages/sheet-bot/src/services/sheetService.ts
+++ packages/sheet-bot/src/services/sheetService.ts
@@ -192,11 +192,11 @@ const scheduleParser = (
         fills: parseValueRange(fills, ([p1, p2, p3, p4, p5]) =>
           Effect.succeed({
             fills: [
-              p1 !== undefined ? String(p1) : undefined,
-              p2 !== undefined ? String(p2) : undefined,
-              p3 !== undefined ? String(p3) : undefined,
-              p4 !== undefined ? String(p4) : undefined,
-              p5 !== undefined ? String(p5) : undefined,
+              p1 ? String(p1) : undefined,
+              p2 ? String(p2) : undefined,
+              p3 ? String(p3) : undefined,
+              p4 ? String(p4) : undefined,
+              p5 ? String(p5) : undefined,
             ] as const,
           }),
         ),
