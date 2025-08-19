d816fa59f55b028e639098ecc3f7d7d262381040
TheerapakG <theerapakg@gmail.com>
2025-08-19 22:27:22 +0700

    feat(sheet-bot): add Room Order feature (command, button, service, client helpers)
    - Add roomOrder chat command and button handler
    
    - Add src/client with sheetApis wrapper for bot to call sheet-apis

---
+++ packages/sheet-bot/src/client/sheetApis.ts
@@ -0,0 +1,22 @@
+import { Effect, pipe } from "effect";
+import type { Server } from "sheet-apis";
+import { WebSocketClient } from "typhoon-client-ws/client";
+import { Config } from "../config";
+
+export class SheetApisClient extends Effect.Service<SheetApisClient>()(
+  "SheetApisClient",
+  {
+    effect: pipe(
+      Effect.Do,
+      Effect.bind("config", () => Config),
+      Effect.bind("client", ({ config }) =>
+        WebSocketClient.create<Server>(config.sheetApisBaseUrl),
+      ),
+      Effect.map(({ client }) => ({
+        get: () => client,
+      })),
+    ),
+    dependencies: [Config.Default],
+    accessors: true,
+  },
+) {}
