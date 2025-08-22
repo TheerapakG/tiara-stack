9698fd3d84cf97af6423fdb72a17098cb10c6cca
TheerapakG <theerapakg@gmail.com>
2025-08-22 17:51:12 +0700

    feat(sheet-bot): add Effect wrappers wrap/wrapOptional and bindify helpers
    Expose .effect/.tap/.map/.bind accessors via wrapper utilities

--- packages/sheet-bot/src/utils/wrapper.ts
+++ packages/sheet-bot/src/utils/wrapper.ts
@@ -99,3 +99,93 @@ export const mapifyOptional = <In, Out, E1, R1>(
         Effect.flatMap((a) => f(transform?.(a))),
       ),
   );
+
+export const bindify = <In, Out, E1, R1>(
+  f: (_: In) => Effect.Effect<Out, E1, R1>,
+) =>
+  Function.dual<
+    <O extends object, N extends string>(
+      name: Exclude<N, keyof O>,
+      transform: (object: NoInfer<O>) => NoInfer<In>,
+    ) => <E2, R2>(
+      self: Effect.Effect<O, E2, R2>,
+    ) => Effect.Effect<
+      { [K in N | keyof O]: K extends keyof O ? O[K] : Out },
+      E1 | E2,
+      R1 | R2
+    >,
+    <O extends object, N extends string, E2, R2>(
+      self: Effect.Effect<O, E2, R2>,
+      name: Exclude<N, keyof O>,
+      transform: (object: NoInfer<O>) => NoInfer<In>,
+    ) => Effect.Effect<
+      { [K in N | keyof O]: K extends keyof O ? O[K] : Out },
+      E1 | E2,
+      R1 | R2
+    >
+  >(
+    3,
+    <O extends object, N extends string, E2, R2>(
+      self: Effect.Effect<O, E2, R2>,
+      name: Exclude<N, keyof O>,
+      transform: (object: NoInfer<O>) => NoInfer<In>,
+    ) =>
+      pipe(
+        self,
+        Effect.bind(name, (a) => f(transform(a))),
+      ),
+  );
+
+export const bindifyOptional = <In, Out, E1, R1>(
+  f: (_: In | undefined) => Effect.Effect<Out, E1, R1>,
+) =>
+  Function.dual<
+    <O extends object, N extends string>(
+      name: Exclude<N, keyof O>,
+      transform?: (object: NoInfer<O>) => NoInfer<In>,
+    ) => <E2, R2>(
+      self: Effect.Effect<O, E2, R2>,
+    ) => Effect.Effect<
+      { [K in N | keyof O]: K extends keyof O ? O[K] : Out },
+      E1 | E2,
+      R1 | R2
+    >,
+    <O extends object, N extends string, E2, R2>(
+      self: Effect.Effect<O, E2, R2>,
+      name: Exclude<N, keyof O>,
+      transform?: (object: NoInfer<O>) => NoInfer<In>,
+    ) => Effect.Effect<
+      { [K in N | keyof O]: K extends keyof O ? O[K] : Out },
+      E1 | E2,
+      R1 | R2
+    >
+  >(
+    (args) => Effect.isEffect(args[0]),
+    <O extends object, N extends string, E2, R2>(
+      self: Effect.Effect<O, E2, R2>,
+      name: Exclude<N, keyof O>,
+      transform?: (object: NoInfer<O>) => NoInfer<In>,
+    ) =>
+      pipe(
+        self,
+        Effect.bind(name, (a) => f(transform?.(a))),
+      ),
+  );
+
+export const wrap = <In, Out, E1, R1>(
+  f: (_: In) => Effect.Effect<Out, E1, R1>,
+) => ({
+  effect: f,
+  tap: tapify(f),
+  map: mapify(f),
+  bind: bindify(f),
+});
+
+export const wrapOptional = <In, Out, E1, R1>(
+  f: (_: In | undefined) => Effect.Effect<Out, E1, R1>,
+) => ({
+  effect: f,
+  tap: tapifyOptional(f),
+  map: mapifyOptional(f),
+  bind: bindifyOptional(f),
+});
