ebe63f2463b395b33069ffa14c26ad301eab7163
TheerapakG <theerapakg@gmail.com>
2025-08-24 00:44:46 +0700

    feat(sheet-bot/utils): introduce expanded wrapper API and classes
    Add effectify/effectifyOptional, tapEffectify/mapEffectify/bindEffectify and Optional variants.
    
    Introduce Wrap and WrapOptional classes exposing sync/effect/tap/map/bind and effect variants for ergonomic composition.

--- packages/sheet-bot/src/utils/wrapper.ts
+++ packages/sheet-bot/src/utils/wrapper.ts
@@ -1,4 +1,14 @@
-import { Effect, Function, pipe } from "effect";
+import { Data, Effect, Function, pipe } from "effect";
+
+export const effectify =
+  <In, Out, E1, R1>(f: (_: In) => Effect.Effect<Out, E1, R1>) =>
+  <E2, R2>(input: Effect.Effect<In, E2, R2>) =>
+    pipe(input, Effect.flatMap(f));
+
+export const effectifyOptional =
+  <In, Out, E1, R1>(f: (_?: In) => Effect.Effect<Out, E1, R1>) =>
+  <E2, R2>(input?: Effect.Effect<In, E2, R2>) =>
+    pipe(input ?? Effect.succeed(undefined), Effect.flatMap(f));
 
 export const tapify = <In, Out, E1, R1>(
   f: (_: In) => Effect.Effect<Out, E1, R1>,
@@ -26,7 +36,7 @@ export const tapify = <In, Out, E1, R1>(
   );
 
 export const tapifyOptional = <In, Out, E1, R1>(
-  f: (_: In | undefined) => Effect.Effect<Out, E1, R1>,
+  f: (_?: In) => Effect.Effect<Out, E1, R1>,
 ) =>
   Function.dual<
     <O>(
@@ -47,6 +57,58 @@ export const tapifyOptional = <In, Out, E1, R1>(
       pipe(
         self,
         Effect.tap((a) => f(transform?.(a))),
+      ),
+  );
+
+export const tapEffectify = <In, Out, E1, R1>(
+  f: (_: In) => Effect.Effect<Out, E1, R1>,
+) =>
+  Function.dual<
+    <O, E2, R2>(
+      transform: (object: NoInfer<O>) => Effect.Effect<NoInfer<In>, E2, R2>,
+    ) => <E3, R3>(
+      self: Effect.Effect<O, E3, R3>,
+    ) => Effect.Effect<O, E1 | E2 | E3, R1 | R2 | R3>,
+    <O, E2, R2, E3, R3>(
+      self: Effect.Effect<O, E3, R3>,
+      transform: (object: NoInfer<O>) => Effect.Effect<NoInfer<In>, E2, R2>,
+    ) => Effect.Effect<O, E1 | E2 | E3, R1 | R2 | R3>
+  >(
+    2,
+    <O, E2, R2, E3, R3>(
+      self: Effect.Effect<O, E3, R3>,
+      transform: (object: NoInfer<O>) => Effect.Effect<NoInfer<In>, E2, R2>,
+    ) =>
+      pipe(
+        self,
+        Effect.tap((a) => pipe(transform(a), Effect.flatMap(f))),
+      ),
+  );
+
+export const tapEffectifyOptional = <In, Out, E1, R1>(
+  f: (_?: In) => Effect.Effect<Out, E1, R1>,
+) =>
+  Function.dual<
+    <O, E2, R2>(
+      transform?: (object: NoInfer<O>) => Effect.Effect<NoInfer<In>, E2, R2>,
+    ) => <E3, R3>(
+      self: Effect.Effect<O, E3, R3>,
+    ) => Effect.Effect<O, E1 | E2 | E3, R1 | R2 | R3>,
+    <O, E2, R2, E3, R3>(
+      self: Effect.Effect<O, E3, R3>,
+      transform?: (object: NoInfer<O>) => Effect.Effect<NoInfer<In>, E2, R2>,
+    ) => Effect.Effect<O, E1 | E2 | E3, R1 | R2 | R3>
+  >(
+    (args) => Effect.isEffect(args[0]),
+    <O, E2, R2, E3, R3>(
+      self: Effect.Effect<O, E3, R3>,
+      transform?: (object: NoInfer<O>) => Effect.Effect<NoInfer<In>, E2, R2>,
+    ) =>
+      pipe(
+        self,
+        Effect.tap((a) =>
+          pipe(transform?.(a) ?? Effect.succeed(undefined), Effect.flatMap(f)),
+        ),
       ),
   );
 
@@ -76,7 +138,7 @@ export const mapify = <In, Out, E1, R1>(
   );
 
 export const mapifyOptional = <In, Out, E1, R1>(
-  f: (_: In | undefined) => Effect.Effect<Out, E1, R1>,
+  f: (_?: In) => Effect.Effect<Out, E1, R1>,
 ) =>
   Function.dual<
     <O>(
@@ -97,6 +159,58 @@ export const mapifyOptional = <In, Out, E1, R1>(
       pipe(
         self,
         Effect.flatMap((a) => f(transform?.(a))),
+      ),
+  );
+
+export const mapEffectify = <In, Out, E1, R1>(
+  f: (_: In) => Effect.Effect<Out, E1, R1>,
+) =>
+  Function.dual<
+    <O, E2, R2>(
+      transform: (object: NoInfer<O>) => Effect.Effect<NoInfer<In>, E2, R2>,
+    ) => <E3, R3>(
+      self: Effect.Effect<O, E3, R3>,
+    ) => Effect.Effect<Out, E1 | E2 | E3, R1 | R2 | R3>,
+    <O, E2, R2, E3, R3>(
+      self: Effect.Effect<O, E3, R3>,
+      transform: (object: NoInfer<O>) => Effect.Effect<NoInfer<In>, E2, R2>,
+    ) => Effect.Effect<Out, E1 | E2 | E3, R1 | R2 | R3>
+  >(
+    2,
+    <O, E2, R2, E3, R3>(
+      self: Effect.Effect<O, E3, R3>,
+      transform: (object: NoInfer<O>) => Effect.Effect<NoInfer<In>, E2, R2>,
+    ) =>
+      pipe(
+        self,
+        Effect.flatMap((a) => pipe(transform(a), Effect.flatMap(f))),
+      ),
+  );
+
+export const mapEffectifyOptional = <In, Out, E1, R1>(
+  f: (_?: In) => Effect.Effect<Out, E1, R1>,
+) =>
+  Function.dual<
+    <O, E2, R2>(
+      transform?: (object: NoInfer<O>) => Effect.Effect<NoInfer<In>, E2, R2>,
+    ) => <E3, R3>(
+      self: Effect.Effect<O, E3, R3>,
+    ) => Effect.Effect<Out, E1 | E2 | E3, R1 | R2 | R3>,
+    <O, E2, R2, E3, R3>(
+      self: Effect.Effect<O, E3, R3>,
+      transform?: (object: NoInfer<O>) => Effect.Effect<NoInfer<In>, E2, R2>,
+    ) => Effect.Effect<Out, E1 | E2 | E3, R1 | R2 | R3>
+  >(
+    (args) => Effect.isEffect(args[0]),
+    <O, E2, R2, E3, R3>(
+      self: Effect.Effect<O, E3, R3>,
+      transform?: (object: NoInfer<O>) => Effect.Effect<NoInfer<In>, E2, R2>,
+    ) =>
+      pipe(
+        self,
+        Effect.flatMap((a) =>
+          pipe(transform?.(a) ?? Effect.succeed(undefined), Effect.flatMap(f)),
+        ),
       ),
   );
 
@@ -137,7 +251,7 @@ export const bindify = <In, Out, E1, R1>(
   );
 
 export const bindifyOptional = <In, Out, E1, R1>(
-  f: (_: In | undefined) => Effect.Effect<Out, E1, R1>,
+  f: (_?: In) => Effect.Effect<Out, E1, R1>,
 ) =>
   Function.dual<
     <O extends object, N extends string>(
@@ -172,20 +286,156 @@ export const bindifyOptional = <In, Out, E1, R1>(
       ),
   );
 
+export const bindEffectify = <In, Out, E1, R1>(
+  f: (_: In) => Effect.Effect<Out, E1, R1>,
+) =>
+  Function.dual<
+    <O extends object, N extends string, E2, R2>(
+      name: Exclude<N, keyof O>,
+      transform: (object: NoInfer<O>) => Effect.Effect<NoInfer<In>, E2, R2>,
+    ) => <E3, R3>(
+      self: Effect.Effect<O, E3, R3>,
+    ) => Effect.Effect<
+      { [K in N | keyof O]: K extends keyof O ? O[K] : Out },
+      E1 | E2 | E3,
+      R1 | R2 | R3
+    >,
+    <O extends object, N extends string, E2, R2, E3, R3>(
+      self: Effect.Effect<O, E3, R3>,
+      name: Exclude<N, keyof O>,
+      transform: (object: NoInfer<O>) => Effect.Effect<NoInfer<In>, E2, R2>,
+    ) => Effect.Effect<
+      { [K in N | keyof O]: K extends keyof O ? O[K] : Out },
+      E1 | E2 | E3,
+      R1 | R2 | R3
+    >
+  >(
+    3,
+    <O extends object, N extends string, E2, R2, E3, R3>(
+      self: Effect.Effect<O, E3, R3>,
+      name: Exclude<N, keyof O>,
+      transform: (object: NoInfer<O>) => Effect.Effect<NoInfer<In>, E2, R2>,
+    ) =>
+      pipe(
+        self,
+        Effect.bind(name, (a) => pipe(transform(a), Effect.flatMap(f))),
+      ),
+  );
+
+export const bindEffectifyOptional = <In, Out, E1, R1>(
+  f: (_?: In) => Effect.Effect<Out, E1, R1>,
+) =>
+  Function.dual<
+    <O extends object, N extends string, E2, R2>(
+      name: Exclude<N, keyof O>,
+      transform?: (object: NoInfer<O>) => Effect.Effect<NoInfer<In>, E2, R2>,
+    ) => <E3, R3>(
+      self: Effect.Effect<O, E3, R3>,
+    ) => Effect.Effect<
+      { [K in N | keyof O]: K extends keyof O ? O[K] : Out },
+      E1 | E2 | E3,
+      R1 | R2 | R3
+    >,
+    <O extends object, N extends string, E2, R2, E3, R3>(
+      self: Effect.Effect<O, E3, R3>,
+      name: Exclude<N, keyof O>,
+      transform?: (object: NoInfer<O>) => Effect.Effect<NoInfer<In>, E2, R2>,
+    ) => Effect.Effect<
+      { [K in N | keyof O]: K extends keyof O ? O[K] : Out },
+      E1 | E2 | E3,
+      R1 | R2 | R3
+    >
+  >(
+    (args) => Effect.isEffect(args[0]),
+    <O extends object, N extends string, E2, R2, E3, R3>(
+      self: Effect.Effect<O, E3, R3>,
+      name: Exclude<N, keyof O>,
+      transform?: (object: NoInfer<O>) => Effect.Effect<NoInfer<In>, E2, R2>,
+    ) =>
+      pipe(
+        self,
+        Effect.bind(name, (a) =>
+          pipe(transform?.(a) ?? Effect.succeed(undefined), Effect.flatMap(f)),
+        ),
+      ),
+  );
+
+class Wrap<In, Out, E1, R1> extends Data.TaggedClass("Wrap")<{
+  readonly f: (_: In) => Effect.Effect<Out, E1, R1>;
+}> {
+  get sync() {
+    return this.f;
+  }
+
+  get effect() {
+    return effectify(this.f);
+  }
+
+  get tap() {
+    return tapify(this.f);
+  }
+
+  get tapEffect() {
+    return tapEffectify(this.f);
+  }
+
+  get map() {
+    return mapify(this.f);
+  }
+
+  get mapEffect() {
+    return mapEffectify(this.f);
+  }
+
+  get bind() {
+    return bindify(this.f);
+  }
+
+  get bindEffect() {
+    return bindEffectify(this.f);
+  }
+}
+
+class WrapOptional<In, Out, E1, R1> extends Data.TaggedClass("WrapOptional")<{
+  readonly f: (_?: In) => Effect.Effect<Out, E1, R1>;
+}> {
+  get sync() {
+    return this.f;
+  }
+
+  get effect() {
+    return effectifyOptional(this.f);
+  }
+
+  get tap() {
+    return tapifyOptional(this.f);
+  }
+
+  get tapEffect() {
+    return tapEffectifyOptional(this.f);
+  }
+
+  get map() {
+    return mapifyOptional(this.f);
+  }
+
+  get mapEffect() {
+    return mapEffectifyOptional(this.f);
+  }
+
+  get bind() {
+    return bindifyOptional(this.f);
+  }
+
+  get bindEffect() {
+    return bindEffectifyOptional(this.f);
+  }
+}
+
 export const wrap = <In, Out, E1, R1>(
   f: (_: In) => Effect.Effect<Out, E1, R1>,
-) => ({
-  effect: f,
-  tap: tapify(f),
-  map: mapify(f),
-  bind: bindify(f),
-});
+) => new Wrap({ f });
 
 export const wrapOptional = <In, Out, E1, R1>(
-  f: (_: In | undefined) => Effect.Effect<Out, E1, R1>,
-) => ({
-  effect: f,
-  tap: tapifyOptional(f),
-  map: mapifyOptional(f),
-  bind: bindifyOptional(f),
-});
+  f: (_?: In) => Effect.Effect<Out, E1, R1>,
+) => new WrapOptional({ f });
