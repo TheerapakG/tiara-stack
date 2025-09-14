743e2645caf118a1ee4a6fc0031130dbfd8729f8
TheerapakG <theerapakg@gmail.com>
2025-09-14 20:07:42 +0700

    build(typhoon-server): remove rollup-plugin-pure and treeshake overrides
    Drop rollup-plugin-pure and custom treeshake settings from `rolldown.config.ts` to avoid unsafe purity annotations and rely on Rolldown defaults.

--- packages/typhoon-server/rolldown.config.ts
+++ packages/typhoon-server/rolldown.config.ts
@@ -3,7 +3,6 @@ import { builtinModules } from "module";
 import path from "pathe";
 import { defineConfig } from "rolldown";
 import { dts } from "rolldown-plugin-dts";
-import { PluginPure } from "rollup-plugin-pure";
 import { fileURLToPath } from "url";
 
 const filePaths = globSync("./src/**/index.ts", { nodir: true }).map((file) =>
@@ -29,16 +28,6 @@ export default defineConfig({
   moduleTypes: {
     ".html": "text",
   },
-  plugins: [
-    dts(),
-    PluginPure({
-      functions: [/^.*$/],
-    }),
-  ],
-  treeshake: {
-    moduleSideEffects: false,
-    annotations: true,
-    unknownGlobalSideEffects: true,
-  },
+  plugins: [dts()],
   external: [...builtinModules, /^node:/, "effect", "valibot", "typhoon-core"],
 });
