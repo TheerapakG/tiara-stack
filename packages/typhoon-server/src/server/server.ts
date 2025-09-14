93c61eddf1ab85ff849af5a3e653b3918042a2d2
TheerapakG <theerapakg@gmail.com>
2025-09-14 19:54:36 +0700

    chore(typhoon-server): log decoded header during stream handling for debugging

--- packages/typhoon-server/src/server/server.ts
+++ packages/typhoon-server/src/server/server.ts
@@ -956,6 +956,7 @@ export class Server<R = never>
         Effect.bind("header", ({ pullDecodedStream }) =>
           pipe(
             HeaderEncoderDecoder.decodeUnknownEffect(pullDecodedStream),
+            Effect.tap((header) => Effect.log("header", header)),
             Effect.either,
           ),
         ),
