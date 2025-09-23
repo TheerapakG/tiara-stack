fe03fa05f28a06417a55ff9a7a8c06549a350e55
TheerapakG <theerapakg@gmail.com>
2025-09-23 16:50:25 +0700

    chore: remove legacy eslint configs and obsolete test files
    Cleanup after oxlint migration and removal of test handlers/commands.

--- packages/sheet-apis/src/server/handler/handler/test/index.ts
+++
@@ -1,9 +0,0 @@
-import { pipe } from "effect";
-import { HandlerContextConfig } from "typhoon-core/config";
-
-import { testOIDCHandler } from "./testOIDC";
-
-export const testHandlerGroup = pipe(
-  HandlerContextConfig.Group.empty(),
-  HandlerContextConfig.Group.add(testOIDCHandler),
-);
