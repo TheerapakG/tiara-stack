0bc55dc6f6994c4b7c9a9a1ed479a4dfe77c1b69
TheerapakG <theerapakg@gmail.com>
2025-10-10 14:32:24 +0700

    refactor(typhoon-core): refine array defaults helpers
    generalize ArrayWithDefault utilities for object tuples
    
    align handler config composition with evolved struct

--- packages/typhoon-core/src/utils/array/withDefault.ts
+++ packages/typhoon-core/src/utils/array/withDefault.ts
@@ -1,39 +1,69 @@
 import { Array, Data, Order, pipe } from "effect";
 
-export class ArrayWithDefault<A> extends Data.TaggedClass("ArrayWithDefault")<{
-  array: A[];
-  default: A;
+export class ArrayWithDefault<
+  S extends ReadonlyArray<object>,
+> extends Data.TaggedClass("ArrayWithDefault")<{
+  array: S;
+  default: Array.ReadonlyArray.Infer<S>;
 }> {}
 
+export const wrap =
+  <S extends ReadonlyArray<object>>(options: {
+    default: Array.ReadonlyArray.Infer<S>;
+  }) =>
+  (array: S) =>
+    new ArrayWithDefault({
+      array,
+      default: options.default,
+    });
+
+export type InferArray<A extends ArrayWithDefault<ReadonlyArray<object>>> =
+  A extends ArrayWithDefault<infer S> ? S : never;
+export type Infer<A extends ArrayWithDefault<ReadonlyArray<object>>> =
+  Array.ReadonlyArray.Infer<InferArray<A>>;
+
+export const toArray = <S extends ArrayWithDefault<ReadonlyArray<object>>>(
+  a: S,
+) => a.array as InferArray<S>;
+export const getDefault = <S extends ArrayWithDefault<ReadonlyArray<object>>>(
+  a: S,
+) => a.default as Infer<S>;
+
 export const zip =
-  <B>(b: ArrayWithDefault<B>) =>
-  <A>(a: ArrayWithDefault<A>) => {
+  <T extends ArrayWithDefault<ReadonlyArray<object>>>(b: T) =>
+  <S extends ArrayWithDefault<ReadonlyArray<object>>>(a: S) => {
     const maxLength = Order.max(Order.number)(
-      Array.length(a.array),
-      Array.length(b.array),
+      Array.length(toArray(a)),
+      Array.length(toArray(b)),
     );
 
     return new ArrayWithDefault({
       array: pipe(
         Array.zip(
-          Array.pad(a.array, maxLength, a.default),
-          Array.pad(b.array, maxLength, b.default),
+          Array.pad(Array.copy(toArray(a)), maxLength, a.default),
+          Array.pad(Array.copy(toArray(b)), maxLength, b.default),
         ),
-        Array.map(([a, b]) => ({ ...a, ...b })),
+        Array.map(([a, b]) => ({ ...a, ...b }) as Infer<S> & Infer<T>),
       ),
-      default: { ...a.default, ...b.default },
+      default: { ...getDefault(a), ...getDefault(b) },
     });
   };
 
 export const map =
-  <A, B>(mapper: (a: A) => B) =>
-  (a: ArrayWithDefault<A>) =>
+  <S extends ArrayWithDefault<ReadonlyArray<object>>, B extends object>(
+    mapper: (a: Infer<S>) => B,
+  ) =>
+  (a: S) =>
     new ArrayWithDefault({
-      array: Array.map(a.array, mapper),
-      default: mapper(a.default),
+      array: Array.map(toArray(a), mapper),
+      default: mapper(getDefault(a)) as Array.ReadonlyArray.Infer<
+        Array.ReadonlyArray.With<InferArray<S>, B>
+      >,
     });
 
 export const zipMap =
-  <A, B>(mapper: (a: A) => B) =>
-  (a: ArrayWithDefault<A>) =>
-    pipe(a, zip(map(mapper)(a)));
+  <S extends ArrayWithDefault<ReadonlyArray<object>>, B extends object>(
+    mapper: (a: Infer<S>) => B,
+  ) =>
+  (a: S) =>
+    pipe(a, zip(pipe(a, map(mapper))));
