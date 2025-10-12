5edcc607ebb76c0ba8689c98351a41bdd8aaf320
TheerapakG <theerapakg@gmail.com>
2025-10-12 13:57:16 +0700

    build(sheet-apis): split tsdown configs

--- packages/sheet-apis/tsdown.config.ts
+++ packages/sheet-apis/tsdown.config.ts
@@ -1,9 +1,16 @@
 import path from "pathe";
 import { defineConfig } from "tsdown";
 
-export default defineConfig({
-  entry: {
-    index: path.resolve(__dirname, "src/index.ts"),
-    runServer: path.resolve(__dirname, "src/runServer.ts"),
+export default defineConfig([
+  {
+    entry: {
+      index: path.resolve(__dirname, "src/index.ts"),
+    },
   },
-});
+  {
+    entry: {
+      runServer: path.resolve(__dirname, "src/runServer.ts"),
+    },
+    noExternal: [/^.*$/],
+  },
+]);
