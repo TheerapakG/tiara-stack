1e45241e98c2104e35c6a73fdcc7e4fe1575a469
TheerapakG <theerapakg@gmail.com>
2025-10-12 22:05:15 +0700

    feat(sheet-apis/calc): add sheet subscription handler

--- packages/sheet-apis/src/server/handler/handler/calc/index.ts
+++ packages/sheet-apis/src/server/handler/handler/calc/index.ts
@@ -3,9 +3,11 @@ import { Context } from "typhoon-server/handler";
 
 import { botCalcHandler } from "./botCalc";
 import { calcHandler } from "./calc";
+import { sheetCalcHandler } from "./sheetCalc";
 
 export const calcHandlerCollection = pipe(
   Context.Collection.empty(),
-  Context.Collection.add(calcHandler),
   Context.Collection.add(botCalcHandler),
+  Context.Collection.add(calcHandler),
+  Context.Collection.add(sheetCalcHandler),
 );
