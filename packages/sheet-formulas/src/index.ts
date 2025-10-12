78a8d5b78a38e6fb97facdf5c5e7f5f57a5d425c
TheerapakG <theerapakg@gmail.com>
2025-10-12 22:57:08 +0700

    feat(sheet-formulas): expose THEECALC2 custom function

--- packages/sheet-formulas/src/index.ts
+++ packages/sheet-formulas/src/index.ts
@@ -1,6 +1,6 @@
 import "../polyfill/classPolyfill";
 
-import { THEECALC as _THEECALC } from "./formulas";
+import { THEECALC as _THEECALC, THEECALC2 as _THEECALC2 } from "./formulas";
 import { onEdit as _onEdit } from "./levi";
 
 /**
@@ -11,6 +11,16 @@ import { onEdit as _onEdit } from "./levi";
  */
 export function THEECALC(...args: Parameters<typeof _THEECALC>) {
   return _THEECALC(...args);
+}
+
+/**
+ * @preserve
+ * Calculate Some Top Teams
+ *
+ * @customfunction
+ */
+export function THEECALC2(...args: Parameters<typeof _THEECALC2>) {
+  return _THEECALC2(...args);
 }
 
 export function onEdit(...args: Parameters<typeof _onEdit>) {
