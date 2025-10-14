109fb0d5ffc55dd01d0875a29ff40cda8839d3d8
TheerapakG <theerapakg@gmail.com>
2025-10-14 21:39:13 +0700

    refactor(sheet-apis/calc): drop legacy handler
    remove unused calc handler config and subscription handler

--- packages/sheet-apis/src/server/handler/config/calc/index.ts
+++ packages/sheet-apis/src/server/handler/config/calc/index.ts
@@ -2,14 +2,12 @@ import { pipe } from "effect";
 import { Handler } from "typhoon-core/server";
 
 import { botCalcHandlerConfig } from "./botCalc";
-import { calcHandlerConfig } from "./calc";
 import { sheetCalcHandlerConfig } from "./sheetCalc";
 
-export { botCalcHandlerConfig, calcHandlerConfig, sheetCalcHandlerConfig };
+export { botCalcHandlerConfig, sheetCalcHandlerConfig };
 
 export const calcHandlerConfigCollection = pipe(
   Handler.Config.Collection.empty(),
-  Handler.Config.Collection.add(calcHandlerConfig),
   Handler.Config.Collection.add(botCalcHandlerConfig),
   Handler.Config.Collection.add(sheetCalcHandlerConfig),
 );
--- packages/sheet-apis/src/server/handler/handler/calc/index.ts
+++ packages/sheet-apis/src/server/handler/handler/calc/index.ts
@@ -2,12 +2,10 @@ import { pipe } from "effect";
 import { Context } from "typhoon-server/handler";
 
 import { botCalcHandler } from "./botCalc";
-import { calcHandler } from "./calc";
 import { sheetCalcHandler } from "./sheetCalc";
 
 export const calcHandlerCollection = pipe(
   Context.Collection.empty(),
   Context.Collection.add(botCalcHandler),
-  Context.Collection.add(calcHandler),
   Context.Collection.add(sheetCalcHandler),
 );
