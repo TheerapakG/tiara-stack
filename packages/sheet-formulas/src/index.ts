bc73bec8032013e47962c694dfe560c397e0e6a0
TheerapakG <theerapakg@gmail.com>
2025-10-19 10:33:19 +0700

    refactor(sheet-formulas): re-export all formulas

--- packages/sheet-formulas/src/index.ts
+++ packages/sheet-formulas/src/index.ts
@@ -1,33 +1,3 @@
 import "../polyfill/classPolyfill";
 
-import {
-  THEECALC as _THEECALC,
-  THEECALC2 as _THEECALC2,
-  onEditInstallable as _onEditInstallable,
-} from "./formulas";
-
-/**
- * @preserve
- * Calculate Some Top Teams
- *
- * @customfunction
- */
-export function THEECALC(...args: Parameters<typeof _THEECALC>) {
-  return _THEECALC(...args);
-}
-
-/**
- * @preserve
- * Calculate Some Top Teams
- *
- * @customfunction
- */
-export function THEECALC2(...args: Parameters<typeof _THEECALC2>) {
-  return _THEECALC2(...args);
-}
-
-export function onEditInstallable(
-  ...args: Parameters<typeof _onEditInstallable>
-) {
-  return _onEditInstallable(...args);
-}
+export * from "./formulas";
