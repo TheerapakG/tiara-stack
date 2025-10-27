180b6c9420f1fc479f4ef82d64d68017d8e3f538
TheerapakG <theerapakg@gmail.com>
2025-10-27 14:55:01 +0700

    feat(typhoon-core/error): add rpc error helpers

--- packages/typhoon-core/src/error/index.ts
+++ packages/typhoon-core/src/error/index.ts
@@ -1,4 +1,5 @@
 export * as Authorization from "./authorization";
 export * as Stream from "./stream";
+export * as Rpc from "./rpc";
 export * as Msgpack from "./msgpack";
 export * as Validation from "./validation";
