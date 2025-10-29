9fb74cc13ddb3b9b3b3401e9da5391d701d07825
TheerapakG <theerapakg@gmail.com>
2025-10-29 10:26:22 +0700

    feat(typhoon-core/error): add argument and db error classes

--- packages/typhoon-core/src/error/index.ts
+++ packages/typhoon-core/src/error/index.ts
@@ -1,4 +1,6 @@
+export * as Argument from "./argument";
 export * as Authorization from "./authorization";
+export * as DB from "./db";
 export * as Stream from "./stream";
 export * as Rpc from "./rpc";
 export * as Msgpack from "./msgpack";
