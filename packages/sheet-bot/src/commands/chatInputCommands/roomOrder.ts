56f2fc1f94767953beb1e9685d8315e8e47894d4
TheerapakG <theerapakg@gmail.com>
2025-11-02 00:10:48 +0700

    refactor(sheet-bot): add debug log for players and teams
    Add logging statement to debug players and teams data in room order manual command

--- packages/sheet-bot/src/commands/chatInputCommands/roomOrder.ts
+++ packages/sheet-bot/src/commands/chatInputCommands/roomOrder.ts
@@ -168,6 +168,7 @@ const handleManual =
                   PlayerService.getTeamsByName,
                 ),
               ),
+              Effect.tap(({ players, teams }) => Effect.log(players, teams)),
               Effect.map(({ players, teams }) => Array.zip(players, teams)),
               Effect.flatMap(
                 Effect.forEach(([player, teams]) =>
