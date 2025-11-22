a001469146ca821e6bb87ebc3522af4feb13b2fc
TheerapakG <theerapakg@gmail.com>
2025-11-22 13:35:28 +0700

    chore(sheet-apis/handler): add debug logging to getZeroGuildRunningChannelById

--- packages/sheet-apis/src/server/handler/handler/guildConfig/getZeroGuildRunningChannelById.ts
+++ packages/sheet-apis/src/server/handler/handler/guildConfig/getZeroGuildRunningChannelById.ts
@@ -37,6 +37,7 @@ export const getZeroGuildRunningChannelByIdHandler = pipe(
         ),
       ),
       Computed.mapEffect(Error.Core.catchParseErrorAsValidationError),
+      Computed.tap((v) => Effect.log(v)),
       Computed.mapEffect(
         Handler.Config.encodeResponseEffect(
           getZeroGuildRunningChannelByIdHandlerConfig,
