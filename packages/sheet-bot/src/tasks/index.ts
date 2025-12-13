4f8fca41916257375675f410ae62418180941f8c
TheerapakG <theerapakg@gmail.com>
2025-12-13 09:53:33 +0700

    refactor(sheet-bot/tasks): simplify task registration
    Remove manual ClientService provision from tasks export since it is now automatically provided by the bot runtime.

--- packages/sheet-bot/src/tasks/index.ts
+++ packages/sheet-bot/src/tasks/index.ts
@@ -1,13 +1,5 @@
 import { autoCheckinTask } from "./autoCheckin";
-import { Effect, pipe } from "effect";
-import { botServices } from "@/services";
 
-export const tasks = [
-  pipe(autoCheckinTask, Effect.provide(botServices)) as Effect.Effect<
-    unknown,
-    unknown,
-    never
-  >,
-];
+export const tasks = [autoCheckinTask];
 
 export { autoCheckinTask };
